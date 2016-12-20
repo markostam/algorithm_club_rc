f47c0dc5bf4ae891c82faded2272642f3188fca0
Marko Stamenovic <markostam@gmail.com>
2016-12-19 20:36:07 -0500

    astar algo first pass

--- aStar/aStar.scala
+++ aStar/aStar.scala
@@ -6,13 +6,10 @@ case class Node (parent: (Int,Int), coord: (Int,Int), g: Float, h: Float){
 
 def nodeOrder (n: Node) = -n.cost
 
-// object MinOrder extends Ordering[Int] {
-//          def compare(x:Int, y:Int) = y compare x
-//        }
-
 val openList = scala.collection.mutable.PriorityQueue.empty(Ordering.by(nodeOrder))
 var closedList : List[Node]= List()
-val firstNode = Node((2,3),(2,3),0,0)
+val goal = (5,5)
+val firstNode = Node((2,3),(2,3),0,l2(0)
 
 
 def l2 (x:(Any,Any),y:(Any,Any)) : Float = {
@@ -23,7 +20,8 @@ def l2 (x:(Any,Any),y:(Any,Any)) : Float = {
            reduce(_+_),0.5).toFloat
 }
 
-def makeChildren (parent: Node, m: DenseMatrix[Float], goal: (Float,Float)) = {
+def makeChildren (parent: Node, m: DenseMatrix[Float], 
+                  goal: (Float,Float)) : List[Node] = {
   // gets neighbors of a point in a DenseMatrix
   val (r,c) = parent.coord
   m(r,c) = 1.0.toFloat
@@ -40,15 +38,27 @@ def makeChildren (parent: Node, m: DenseMatrix[Float], goal: (Float,Float)) = {
     parent.g+l2(parent.coord,newCoord),l2(goal,newCoord)))
 }
 
+def createPath (node: Node, closedList:List[Node], 
+                oldPath: List[(Float,Float)] = List()) : List[(Float,Float)] = {
+  val newList : List[(Float,Float)] = List(node.coord).map(x => (x._1.toFloat,x._2.toFloat))
+  val newPath = oldPath ++ newList
+  if (node.g == 0) newPath
+  else {
+    val nextNode = closedList.filter(_.coord == node.parent)(0)
+    createPath(nextNode,closedList,newPath)
+  }
+}
 
 def aStar (openList : scala.collection.mutable.PriorityQueue[Node], 
            closedList :List[Node], m: DenseMatrix[Float],
-           start: (Int,Int), goal: (Float,Float)) = {
+           start: (Int,Int), goal: (Float,Float)) : List[(Float,Float)]= {
   val q = openList.dequeue()
   val children = makeChildren(q,m,goal)
   if (children.exists(_.coord == goal)) {
     // return shortest path
-    closedList
+    println("a* done :)")
+    val goalNode = children.filter(_.coord == goal).head
+    createPath(goalNode,closedList)
   }
   else {
     val testChildren = children.filterNot(x => openList.
@@ -56,10 +66,22 @@ def aStar (openList : scala.collection.mutable.PriorityQueue[Node],
       filterNot(x => closedList.
       exists(y => x.coord == y.coord & x.cost > y.cost))
     testChildren.foreach(x => openList.enqueue(x))
-    openList
+    closedList :+ q
+    aStar(openList,closedList,m,start,goal)
   }
-  closedList :+ q
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 xs.map(x => ys.map(y => (x,y))).flatten.
 filter(x => x._1.coor == x._2.coor & x._1.f < x._2.f).
